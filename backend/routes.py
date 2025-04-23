fabc445449f9c15ec2bbc3d20c80328431e5cf78
Omarb28 <omarb28@gmail.com>
2025-04-23 05:00:22 -0400

    Add Get Picture routes

--- backend/routes.py
+++ backend/routes.py
@@ -7,19 +7,19 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 json_url = os.path.join(SITE_ROOT, "data", "pictures.json")
 data: list = json.load(open(json_url))
 
+
 ######################################################################
 # RETURN HEALTH OF THE APP
 ######################################################################
-
 
 @app.route("/health")
 def health():
     return jsonify(dict(status="OK")), 200
 
+
 ######################################################################
 # COUNT THE NUMBER OF PICTURES
 ######################################################################
-
 
 @app.route("/count")
 def count():
@@ -33,23 +33,29 @@ def count():
 ######################################################################
 # GET ALL PICTURES
 ######################################################################
+
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return jsonify(data)
+
 
 ######################################################################
 # GET A PICTURE
 ######################################################################
 
-
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
-
+    for picture in data:
+        if picture['id'] == id:
+            return jsonify(picture), 200
+        else:
+            return {"message": f"Picture with ID {id} is not found."}, 404
+    
 
 ######################################################################
 # CREATE A PICTURE
 ######################################################################
+
 @app.route("/picture", methods=["POST"])
 def create_picture():
     pass
@@ -58,7 +64,6 @@ def create_picture():
 # UPDATE A PICTURE
 ######################################################################
 
-
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
     pass
@@ -66,6 +71,8 @@ def update_picture(id):
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
+
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
     pass
+
